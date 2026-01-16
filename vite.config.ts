4dc5bd7937c0a54d524c549b12031b19d8a6b92f
Mirrer1 <alsejr1004@gmail.com>
2026-01-17 00:17:23 +0900

    feat: production 콘솔 제거 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src'),
     },
+  },
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
   },
   server: {
     proxy: {
@@ -19,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
